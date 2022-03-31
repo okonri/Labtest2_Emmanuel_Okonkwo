cfb4c1eac29502bb69a3f917fdda17eab1059192
okonri <ikeclemens@gmail.com>
2022-03-30 20:24:54 -0400

    still working

---
+++ src/assets/dump.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS placestable(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lat TEXT, 
+    lng TEXT
+);
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Justin Bieber', 'Yummy');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Jonas Brothers', 'What A Man Gotta Do');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Life Is Good', 'Future');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Lauv', 'Tattoos Together');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Heavy Steppers', 'Whateva');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'DigDat 2020', 'Ei8ht Mile');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Blackbear', 'me & ur ghost');
+INSERT or IGNORE INTO songtable( lat, lng) VALUES ( 'Hailee Steinfeld', 'Wrong Direction');
